83b417d4b2169d5e3f827b9507a8a27d2d5faa73
buckballs <buckballs01@gmail.com>
2016-09-16 11:45:17 +0100

    Added some comments to task 3
    Still working on it.

--- Python versions/Task 3.py
+++ Python versions/Task 3.py
@@ -1,33 +1,33 @@
-def readfile():
-    sentencelist = []
-    attempt = "no"
-    while attempt == "no":
-        filename = input("Enter file name or type cancel. If you used this program to create the file, the filename will be whatever you called it plus 'words' on the end of it. (leave out the txt): ")
-        if filename == "cancel":
+def readfile(): #creates the function which will be used to read compressed files.
+    sentencelist = [] #creates a blank array which will be added to later.
+    attempt = "no" #creates a variable witht he value of "no" to be used for a while loop
+    while attempt == "no": #creates a loop so if an invalid input has been entered by the user, it loops back to the begining
+        filename = input("Enter file name or type cancel. If you used this program to create the file, the filename will be whatever you called it plus 'words' on the end of it. (leave out the txt): ") #user enters the name of the file they wish to read here
+        if filename == "cancel": #if the user enters cancel it takes them back to the starting screen
+            menu() #starting screen where user enters what they wish to do (read, write or compress)
+        elif filename == "Cancel": #again the cancel option but with a captiol C to support case senstivity
             menu()
-        elif filename == "Cancel":
-            menu()
-        try:
-            with open(filename + '.txt', 'r') as f:
-                words = f.readlines()
-                for word in words:
-                    if word not in sentencelist:
-                        word = word.strip('\n')
-                        sentencelist.append(word)
-                        attempt = "yes"
-        except FileNotFoundError:
-            print("Oops! Doesn't look like there's a file with that name (not locally anyway).")
-            attempt = "no"
-    sentencestring = " ".join(sentencelist)
-    print("The following sentence has been constucted: \n \n" + sentencestring)
-    menu()
+        try: #runs this peice of code within an error exception
+            with open(filename + '.txt', 'r') as f: #opens the file that the user identified previously in the filename variable
+                words = f.readlines() #sets the varaible words to have all the data in the file
+                for word in words: #for each word in the file
+                    if word not in sentencelist: #if any of the words it goes through is not in the empty array sentencelist
+                        word = word.strip('\n') #removes the newline from the word to prevent logical errors
+                        sentencelist.append(word) #adds the word to the previously empty array
+                        attempt = "yes" #ends the while loop so the code continues to the user being takent to the menu screen
+        except FileNotFoundError: #if this error occurs, do the following
+            print("Oops! Doesn't look like there's a file with that name (not locally anyway).") #prints that a file was not found
+            attempt = "no" #attempt remains "no" so it loops back to asking for the file name until they enter a valid response or cancel and return to the main menu
+    sentencestring = " ".join(sentencelist)#assigns a space character and the the sentence in the sentence list to this variable
+    print("The following sentence has been constucted: \n \n" + sentencestring) #outputs the sentence that was put together
+    menu() #returns user to main screen
     
-def compressfile():
-    sentence = input("Enter sentence to compress or type cancel \n > ")
-    sentencename = input("Enter the sentences' name or type cancel \n > ")
-    if sentence == "cancel" or sentencename == "cancel":
+def compressfile(): #the following code will make the compress file function
+    sentence = input("Enter sentence to compress or type cancel \n > ") #user enters the sentence they wish to compress or returns to the main menu
+    sentencename = input("Enter the sentences' name or type cancel \n > ") #user enters the name they wish to assign to the sentence, this will later be used for the written file name
+    if sentence == "cancel" or sentencename == "cancel": #if the user enters cancel in either of the entries, take the user back to the main screen
         menu()
-    elif sentence == "Cancel" or sentencename == "Cancel":
+    elif sentence == "Cancel" or sentencename == "Cancel": #added capitol C's to help non-case senstivity
         menu()
     sentencelist = sentence.split()
     words = {}
