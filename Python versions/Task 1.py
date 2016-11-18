814301833930d6fa48ceebf9b5a369c9910a5fb3
buckballs <buckballs01@gmail.com>
2016-11-18 17:09:41 +0000

    Added punctuation check

--- Python versions/Task 1.py
+++ Python versions/Task 1.py
@@ -1,12 +1,15 @@
+import string #acts like a dictionary so later I can use short phrases which can be lookedup in this
 empty = True
 while empty is True: #just a while loop to make sure the user actually enters somthing as entering nothing could cause errors..
-    sentence = input("Enter sentence: \n>")#user enters sentence which is stored in the variable as sentence.
-    if sentence == "":
-        print("You didn't enter anything...")
-    if sentence !="":
-        empty = False #ends while loop
+    sentence = input("Enter sentence (without punctuation): \n>")#user enters sentence which is stored in the variable as sentence.
+    if not sentence: #if the sentence is left blank (nothing was entered)
+        print("You didn't enter anything...") #output that nothing was entered
+    elif any(punctuation in sentence for punctuation in string.punctuation): #or if any item in the string.punctuation list is in the sentence, output that punctuation was found.
+        print("Punctuation found!")
+    else: #if the sentence passes all of these conditions then
+        empty = False #end the while loop and procede
 sentence = sentence.lower() #turns the sentence into all lower case to prevent case senstivity.
-sentencelist = sentence.split() #splits the sentence into individual words and stores it in this variable as a list.
+sentencelist = sentence.split()#splits the sentence into individual words and stores it in this variable as a list.
 search = input("Word to find:\n>") #user enters word they wish to find which is also stored as a variable.
 search = search.lower() #again, the word the user wishes to search is turned into lower case to match the sentence, avoiding case senstivity.
 position = [i for i, found in enumerate(sentencelist) if found == search] #this loop goes through each induvidual word in the sentence list, and see's if it matches with the search variable. The use of enumerate is so that the loop will work throughout the entire sentence, not just stopping at the first word it finds.
@@ -18,4 +21,3 @@ if not position: #basically, if no positions are found, then output it no word f
 else: #self explanitory, if the word was found, output the success, and it's positions.
     print("The word,", search, ", was found at position"+ plural, position, ".")
 input() #creates a break so the user can press enter when they wish to close the program.
-
