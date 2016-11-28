3d90f4c34ed48dc9ec14d3528dfdbabe27ac7ba8
buckballs <buckballs01@gmail.com>
2016-11-28 19:45:49 +0000

    changed a comment to make more sense

--- Python versions/Task 1.py
+++ Python versions/Task 1.py
@@ -12,7 +12,7 @@ sentence = sentence.lower() #turns the sentence into all lower case to prevent c
 sentencelist = sentence.split()#splits the sentence into individual words and stores it in this variable as a list.
 search = input("Word to find:\n>") #user enters word they wish to find which is also stored as a variable.
 search = search.lower() #again, the word the user wishes to search is turned into lower case to match the sentence, avoiding case senstivity.
-position = [i for i, found in enumerate(sentencelist) if found == search] #this loop goes through each induvidual word in the sentence list, and see's if it matches with the search variable. The use of enumerate is so that the loop will work throughout the entire sentence, not just stopping at the first word it finds.
+position = [i for i, found in enumerate(sentencelist) if found == search] #For every word in sentencelist that matches search, it will add the index (position) of that word to the position list.
 if len(position) >1: plural = "s" #used to help the following output make more sense. basically, if the word has been found more than one time, the sentence will say "positions" instead of "position"
 else:
     plural = "" #sets the variable plural as nothing so it knows not to add an s on the end of positions.
